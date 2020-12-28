ebb2350005a64a140f1b31201a3711efa2b7bb25
savvasenok <nightowlsavva@gmail.com>
2020-12-28 22:59:19 +0200

    Added empty login fragment. Updated logo

---
+++ app/src/main/java/xyz/savvamirzoyan/trueithubtalks/fragment/LoginFragment.kt
@@ -0,0 +1,21 @@
+package xyz.savvamirzoyan.trueithubtalks.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import xyz.savvamirzoyan.trueithubtalks.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        val binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
