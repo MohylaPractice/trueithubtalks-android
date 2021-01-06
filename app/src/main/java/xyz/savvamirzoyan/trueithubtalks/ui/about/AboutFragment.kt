4e13d3de03cf75a5b07e4741b8418e23db519d09
savvasenok <nightowlsavva@gmail.com>
2021-01-06 14:20:37 +0200

    Added some text and buttons to AboutFragment.kt

--- app/src/main/java/xyz/savvamirzoyan/trueithubtalks/ui/about/AboutFragment.kt
+++ app/src/main/java/xyz/savvamirzoyan/trueithubtalks/ui/about/AboutFragment.kt
@@ -1,19 +1,34 @@
 package xyz.savvamirzoyan.trueithubtalks.ui.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import xyz.savvamirzoyan.trueithubtalks.R
+import xyz.savvamirzoyan.trueithubtalks.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
+
+    private lateinit var binding: FragmentAboutBinding
+
+    private val apiSourceLink = "https://github.com/savvasenok/trueithubtalks-backend"
+    private val appSourceLink = "https://github.com/savvasenok/trueithubtalks-android"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val binding = FragmentAboutBinding.inflate(inflater, container, false)
+
+        val apiIntent = Intent(Intent.ACTION_VIEW, Uri.parse(apiSourceLink))
+        val appIntent = Intent(Intent.ACTION_VIEW, Uri.parse(appSourceLink))
+
+        binding.buttonApiSource.setOnClickListener { startActivity(apiIntent) }
+        binding.buttonAppSource.setOnClickListener { startActivity(appIntent) }
+
+        return binding.root
     }
 }
